699cca30244022b32b1dbe9ca979e640567eb789
immualifin <mimualifin22@gmail.com>
2025-11-10 22:20:55 +0700

    create migration, model and configure

---
+++ app/Models/CateringBonus.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CateringBonus extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "name",
+        "photo",
+        "category_id",
+        "catering_package_id",
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function cateringPackage(): BelongsTo
+    {
+        return $this->belongsTo(CateringPackage::class);
+    }
+}
---
+++ app/Models/CateringTier.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class CateringTier extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "name",
+        'photo',
+        'tagline',
+        'quantity',
+        'price',
+        'duration',
+        'catering_package_id',
+    ];
+
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(TierBenefit::class);
+    }
+
+    public function cateringPackage(): BelongsTo
+    {
+        return $this->belongsTo(CateringPackage::class);
+    }
+}
